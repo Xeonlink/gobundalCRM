bb8d6809bf53017c8c6e2c52cd0ea64eb3868c6e
jimin <dia0114@naver.com>
2023-07-26 16:37:30 +0900

    fix deplot errros 3

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -41,7 +41,7 @@ export default function Page(props: PageProps<any, SearchParams>) {
   const { url = "/admin/orders" } = searchParams;
 
   const naviate = useRouter();
-  useAuth();
+  const auth = useAuth();
   const [status, setStatus] = useState<Status>("NoError");
 
   const [credentials, actions] = useTypeSafeReducer(defaultCredentials, {
